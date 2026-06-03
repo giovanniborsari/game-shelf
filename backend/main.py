83409f059bc693b989966309915c30dc593e07ed
Giovanni Macri Borsari <ssborsari@gmail.com>
2026-06-03 18:02:39 -0500

    Endpoint get game by id, and get game by searching name

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,8 @@ from pydantic import BaseModel
 from datetime import datetime
 import random
 
-class Item(BaseModel):
+class FormatedItem(BaseModel):
+    game_id: int
     game_title: str
     game_platforms: str | None
     game_genre: str | None
@@ -21,13 +22,19 @@ app = FastAPI()
 def root():
     return {"message": "Game Shelf is running!"} 
 
+@app.post("/auth/register")
+
+@app.post("/auth/login")
+
 @app.get("/items/")
-def items(skip: int = 0, limit: int = 36):
+def get_items(page:int = 1, limit: int = 36):
+    skip = (page - 1) * limit
     database = SessionLocal()
     items = database.query(Items).offset(skip).limit(limit).all()
     formatted_items = []
     for item in items:
-        new_item = Item(
+        new_item = FormatedItem(
+            game_id = item.item_id, #type:ignore
             game_title = item.item_name, #type: ignore
             game_platforms = item.platform, #type: ignore
             game_genre = item.genre, #type: ignore
@@ -40,6 +47,73 @@ def items(skip: int = 0, limit: int = 36):
         
         formatted_items.append(new_item) #type: ignore
 
+    total = database.query(Items).count()
+
     database.close()
 
-    return formatted_items
+    return {
+    "items": formatted_items,
+    "total": total,
+    "page": page,
+    "pages": (total + limit - 1) // limit
+    }
+
+@app.get("/items/get_id/{id}")
+def get_item_id(id: int):
+    database = SessionLocal()
+    item = database.query(Items).filter(Items.item_id == id).first()
+
+    if item is not None:
+        desired_game = FormatedItem(
+            game_id = item.item_id, #type:ignore
+            game_title = item.item_name, #type: ignore
+            game_platforms = item.platform, #type: ignore
+            game_genre = item.genre, #type: ignore
+            game_rating = item.rating, #type: ignore
+            game_category = item.categories, #type: ignore
+            game_release_date = item.release_date, #type: ignore
+            game_description = item.description, #type: ignore
+            game_cover = item.cover #type: ignore
+        )
+        return desired_game
+    else:
+        return "Item not found, do you want to add a new game to our database?"
+
+@app.get("/items/search/{search}")
+def get_item_search(search: str, page:int = 1, limit: int = 36):
+    skip = (page - 1) * limit
+    database = SessionLocal()
+    items = database.query(Items).filter(Items.item_name.ilike(f"%{search}%"))\
+        .offset(skip).limit(limit).all()
+    formatted_items = []  
+
+    if items: 
+        for item in items:
+            new_item = FormatedItem(
+            game_id = item.item_id, #type:ignore
+            game_title = item.item_name, #type: ignore
+            game_platforms = item.platform, #type: ignore
+            game_genre = item.genre, #type: ignore
+            game_rating = item.rating, #type: ignore
+            game_category = item.categories, #type: ignore
+            game_release_date = item.release_date, #type: ignore
+            game_description = item.description, #type: ignore
+            game_cover = item.cover #type: ignore
+            )
+        
+            formatted_items.append(new_item) #type: ignore
+
+        total = database.query(Items)\
+            .filter(Items.item_name.ilike(f"%{search}%")).count()
+
+        database.close()
+
+        return {
+        "items": formatted_items,
+        "total": total,
+        "page": page,
+        "pages": (total + limit - 1) // limit
+        }   
+    else:
+       database.close()
+       return "Item not found, do you want to add a new game to our database?"                                                                             
