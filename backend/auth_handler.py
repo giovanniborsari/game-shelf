071a67340c01ce7da200ba18feeb230e65db33e3
Giovanni Macri Borsari <ssborsari@gmail.com>
2026-06-04 20:48:50 -0500

    Encode and Decode JWT implemented

--- backend/auth_handler.py
+++ backend/auth_handler.py
@@ -3,13 +3,37 @@ from typing import Dict
 import jwt
 from dotenv import load_dotenv
 import os
+import binascii
 
 load_dotenv() #reads .env file and loads it to python
 
-SECRET_CLIENT = os.getenv("CLIENT_ID")
-SECRET_ALG = os.getenv("ALGORITHM")
+SECRET_JWT = os.getenv("S_JWT")
+SECRET_ALG = os.getenv("S_ALGORITHM")
 
 def ret_token(token: str):
     return {
         "access_token": token
     }
+
+def encode_jwt(user_id: int) -> Dict[str,str]:
+
+    payload = {
+        "user_id": str(user_id),
+        "expires": time.time() + 600
+    }
+
+    #Creates a safe url string
+    token = jwt.encode(payload, SECRET_JWT, algorithm = SECRET_ALG) #type:ignore
+
+    return ret_token(token)
+
+def decode_jwt(token: str) -> dict:
+    try:
+        decoded = jwt.decode(token, SECRET_JWT, algorithms=[SECRET_ALG]) #type: ignore
+        if decoded["expires"] >= time.time():
+            return decoded
+        else:
+            return None #type: ignore
+    except:
+        return {}
+        
