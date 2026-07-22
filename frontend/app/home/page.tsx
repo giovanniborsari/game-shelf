b137d37da0e90186d2a123de0b7a0fb25971ec97
Giovanni Macri Borsari <ssborsari@gmail.com>
2026-07-21 22:03:59 -0500

    genre row for home page started, generates random genre

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -1,4 +1,5 @@
 import ConsoleRow from "../components/ConsoleRow";
+import GenreRow from "../components/GenreRow";
 import TopBar from "../components/TopBar";
 
 export default function Home() {
@@ -6,6 +7,8 @@ export default function Home() {
     <div className="min-h-screen bg-gray-900 flex flex-col">
         <TopBar/>
         <ConsoleRow/>
+        <GenreRow/>
+        <GenreRow/>
     </div>
 
   );
