41b6caf2ceb3e528c2ba88d578d90ab489078d59
Giovanni Macri Borsari <ssborsari@gmail.com>
2026-07-25 14:16:29 -0500

    login implemented

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useState } from "react";
+import TopBar from "../components/TopBar";
+import { useRouter } from "next/navigation";
+
+export default function Login(){
+    const router = useRouter();
+    const [error, setError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return(
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center"> 
+    <TopBar/>
+    <div className="border-2 border-emerald-400 flex flex-col mt-10 p-3 
+    items-center">
+    <h1 className="text-white text-2xl font-bold mr-auto p-2">Username</h1>
+    <input 
+    type="text"
+    value={username}
+    onChange={(e) => setUsername(e.target.value)}
+    className="bg-white min-w-xl min-h-10 text-black text-xl font-semibold 
+    p-2"
+    placeholder="Username"
+    ></input>
+    <br></br>
+    <h1 className="text-white text-2xl font-bold mr-auto p-2">Password</h1>
+    <input 
+    type="password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    className="bg-white min-w-xl min-h-10 text-black text-xl font-semibold 
+    p-2"
+    placeholder="Password"
+    ></input>
+    <br></br>
+    <button 
+    onClick={() => {
+    fetch("http://localhost:8000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username:username,
+        password:password
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.access_token) {
+        localStorage.setItem("token", data.access_token);
+        router.push("/");
+      } else {
+        setError(data.Error);
+      }
+    })
+    }}
+    className="flex-1 bg-emerald-400 
+    hover:bg-emerald-500 text-gray-900 font-bold py-2 px-4 rounded 
+    transition-colors min-w-xl min-h-10">
+    Login
+    </button>
+    {error && <p className="text-red-500 mt-2">{error}</p>}
+    <br></br>
+    <button type="submit" className="flex-1 bg-emerald-400 
+    hover:bg-emerald-500 text-gray-900 font-bold py-2 px-4 rounded 
+    transition-colors min-w-xl min-h-10">
+    Register
+    </button>
+    </div>
+    </div>
+
+    )
+}
