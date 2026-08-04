2cfcdac4e871c1ea449c6230ee6dd257117dabda
Giovanni Macri Borsari <ssborsari@gmail.com>
2026-08-04 18:39:19 -0500

    now website opens in homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,5 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-900">
-      <h1 className="text-3xl font-bold text-emerald-400 p-6">GameShelf</h1>
-      <hr className="text-gray-500"></hr>
-      <br></br>
-    </div>
-  );
+import { redirect } from "next/navigation";
+
+export default function Page() {
+  redirect("/home");
 }
