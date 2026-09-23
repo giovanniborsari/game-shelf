218a26c7286191b1b622545cd862bfc58e505863
Giovanni Macri Borsari <ssborsari@gmail.com>
2026-09-23 13:59:07 -0500

    bottom bar implemented on mobile

--- frontend/app/components/BottomBar.tsx
+++ frontend/app/components/BottomBar.tsx
@@ -1,17 +1,17 @@
 export default function BottomBar() {
 
 return (
-    <footer className="flex flex-row h-24 items-center w-full bg-gray-900 
+    <footer className="flex flex-row h-auto max-h-24 items-center w-full bg-gray-900 
     mt-auto">
     <div className="flex flex-row mr-auto ml-auto p-2">
       <label className="text-xl text-white">
-        Created by: </label>
+        By: </label>
       <a href="https://github.com/giovanniborsari" 
       className="text-xl font-bold text-gray-300 hover:text-emerald-400
       ml-2">
         Giovanni Macri Borsari</a>
       <img src="/GitHub_Lockup_White_Clearspace.png"
-      className="w-25 ml-2"></img>
+      className="max-w-25 w-auto ml-2"></img>
     </div>
     </footer>
   );
